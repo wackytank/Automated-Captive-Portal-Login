423c988038319969f15182d20abd2185fba077d8
wackytank <31746626+wackytank@users.noreply.github.com>
2017-10-29 06:06:03 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,13 @@
+from selenium import  webdriver
+
+driver = webdriver.Chrome('/home/django/Desktop/chromedriver')
+
+driver.get("http://172.16.40.5:8090/httpclient.html")
+
+user = driver.find_element_by_name("username")
+user.send_keys("trump_user")
+password = driver.find_element_by_name("password")
+password.send_keys("trump_pw")
+
+driver.find_element_by_name("btnSubmit").click()
+driver.close()
